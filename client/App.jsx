c915913365ace0eaefe5b9c7a372032f97ab6e7e
flowcore-aidev <w63towing@gmail.com>
2025-07-23 18:46:58 -0700

    Add files via upload

---
+++ client/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Lydell Deploy Console</h1>
+      <p>Type what you want to build and deploy.</p>
+    </div>
+  );
+}
