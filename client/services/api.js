5bcce9e4e81653a225d43c4ef8f968869227a956
flowcore-aidev <w63towing@gmail.com>
2025-07-25 09:31:42 -0700

    Update api.js

--- client/services/api.js
+++ client/services/api.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "https://lydell-docker-deploy.onrender.com";
+const BACKEND_URL = "https://deploybackend-rq7i.onrender.com";
 
 export const sendPrompt = async (prompt) => {
   try {
